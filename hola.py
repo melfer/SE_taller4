c789410758b3fd5d20da5b6ce3df74a5b4f0050e
melfer <melferalvarezsierra2015@gmail.com>
2020-09-26 23:20:54 -0500

    se agrega --melfer como argumento del programa

--- hola.py
+++ hola.py
@@ -1,3 +1,8 @@
-def say_hello(name="World"):
- print("Hello ", name, "!")
-say_hello()
+import argparse
+def say_hello(melfer="World"):
+ print("Hello ", melfer, "!")
+parser = argparse.ArgumentParser(description='Say hello.')
+parser.add_argument('--melfer', type=str, required=False,
+ metavar='melfer', help='your melfer', default='World')
+args = parser.parse_args()
+say_hello(args.melfer)
